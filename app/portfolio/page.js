892178655b6e7298671d4ccb7c581659c1c2b2e6
saad-mamun <mamunali88991@gmail.com>
2025-05-19 17:37:49 +0600

    ImageOptimize

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,7 +1,19 @@
+import FakeImage from '@/public/next.svg'
+import Image from 'next/image';
+
 const  Portfolio =() => {
   return (
     <div>
-      This is portfolio paga
+      This is portfolio page
+      <Image src={FakeImage} alt='Next text'
+      width={300}
+      />
+      <Image src='https://images.pexels.com/photos/3861959/pexels-photo-3861959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+       alt='Image Address'
+       width={400}
+       height={400}
+       />
+     
     </div>
   )
 }
@@ -14,3 +26,4 @@ export const  generateMetadata = () => {
     description:"All about of Portfolio"
   }
 }
+
