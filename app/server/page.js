88cca15aa9fefcf8e67595abfe71e5db5641a2e5
saad-mamun <mamunali88991@gmail.com>
2025-05-31 16:31:29 +0600

    serverDatafetch

--- app/server/page.js
+++ app/server/page.js
@@ -1,5 +1,21 @@
-const Server = () => {
-    console.log("Hello from the (Server component)")
-    return <div>Server Component</div>
+async function fetchData() {
+    let data = await fetch('https://jsonplaceholder.typicode.com/posts')
+    data = await data.json()
+    return data
 }
-export default Server
+
+const Data = async () => {
+    let products = await fetchData();
+
+    console.log(products)
+    return(
+        <div>
+            <ul>
+                {products?.map((p) => (
+                    <li key={p.id}>{p.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+export default Data
